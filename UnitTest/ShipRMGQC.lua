97c240a9f2f7d3303b8ccb47cdc1dbff1fc9ed8a
huuhghhgyg <huuhghhgyg@outlook.com>
2023-10-29 22:19:42 +0800

    [test] 添加rmgqc和ship的单元测试

---
+++ UnitTest/ShipRMGQC.lua
@@ -0,0 +1,98 @@
+require('rmgqc')
+require('ship')
+
+-- 控制器
+scene.setenv({
+    grid = 'plane'
+})
+
+-- 参数设置
+local simv = 5 -- 仿真速度
+local actionObjs = {} -- 动作队列声明
+local roadList = {} -- 道路列表
+
+-- 程序控制
+local runcommand = true
+
+-- 初始时间
+local t = os.clock()
+local dt = 0
+
+-- todo: 此处的控制程序修改过，可能需要更新到正式使用的文件中
+function update()
+    -- 刷新时间间隔
+    dt = (os.clock() - t) * simv
+    t = os.clock()
+
+    -- 计算最大更新时间
+    local maxstep = dt
+    for i = 1, #actionObjs do
+        if #actionObjs[i].tasksequence > 0 then
+            maxstep = math.min(maxstep, actionObjs[i]:maxstep())
+        end
+    end
+
+    -- 执行更新
+    for i = 1, #actionObjs do
+        actionObjs[i]:executeTask(dt)
+    end
+
+    -- 回收
+    for i = 1, #actionObjs do
+        local obj = actionObjs[i]
+
+        if obj.type == "agv" and #obj.tasksequence == 0 then
+            recycle(obj)
+            table.remove(actionObjs, i)
+            break -- 假设每次同时只能到达一个，因此可以中止
+        end
+    end
+
+    -- 绘图
+    runcommand = scene.render()
+
+    -- 下一次更新
+    if runcommand then
+        coroutine.queue(dt, update)
+    end
+end
+
+function recycle(obj)
+    if obj.type == "agv" then
+        if obj.container ~= nil then
+            obj.container:delete()
+        end
+        obj:delete()
+    end
+end
+
+-- local rmgqc = RMGQC({-16, 0, 130})
+local rmgqc = RMGQC({0, 0, 0})
+local ship = Ship({8, 9, 2}, rmgqc.berthPosition)
+
+-- ship填充集装箱
+ship:fillAllContainerPositions()
+
+-- 绑定Ship
+rmgqc:bindShip(ship)
+
+-- 添加任务
+local tbay = 3
+local trow = 4
+local tlevel = 2
+-- 取下
+rmgqc:addtask({"move2", rmgqc:getcontainercoord(tbay, -1, rmgqc.toplevel)})
+rmgqc:addtask({"move2", rmgqc:getcontainercoord(tbay, trow, rmgqc.toplevel)})
+rmgqc:addtask({"move2", rmgqc:getcontainercoord(tbay, trow, tlevel)})
+rmgqc:addtask({"attach", {tbay, trow, tlevel}})
+rmgqc:move2readyPos(tbay, trow)
+rmgqc:addtask({"move2", rmgqc:getcontainercoord(tbay, -1, 1)})
+rmgqc:addtask({"detach", nil})
+
+-- 装载
+rmgqc:lift2ship(tbay, trow, tlevel)
+
+-- 加入动作队列
+table.insert(actionObjs, rmgqc)
+
+update()
