ba83644466a5097d63bc23e9ebaaaada53b5bf49
huuhghhgyg <huuhghhgyg@outlook.com>
2023-11-05 22:33:03 +0800

    [test] 添加main
    并将旧main移动到example/

---
+++ examples/main.old.lua
@@ -0,0 +1,145 @@
+scene.setenv({
+    grid = 'plane'
+})
+
+-- 程序控制
+local runcommand = true
+
+-- 参数设置
+local simv = 10 -- 仿真速度
+local actionobj = {} -- 动作队列声明
+local agvSummonSpan = 46 -- agv生成间隔
+
+-- 引用组件
+require('rmg')
+require('agv')
+require('cy')
+require('rmgqc')
+require('ship')
+
+-- 创建堆场
+local cy = CY({19.66 / 2, 51.49 / 2}, {-19.66 / 2, -51.49 / 2}, 3)
+
+-- 分配堆场给场桥
+local rmg = RMG(cy, actionobj)
+table.insert(actionobj, rmg)
+
+local rmgqc = RMGQC() -- 获取岸桥
+rmg.nextstep = rmgqc -- 设置岸桥(仅在main中发现这样的作法)
+table.insert(actionobj, rmgqc)
+
+local ship = SHIP(rmgqc)
+ship:initqueue()
+
+local line = scene.addobj("polyline", {
+    vertices = {cy.summon[1], cy.summon[2], cy.summon[3], ship.exit[1], ship.exit[2], ship.exit[3]},
+    color = "black"
+})
+local pt = scene.addobj("points", {
+    vertices = {cy.summon[1], cy.summon[2], cy.summon[3], ship.exit[1], ship.exit[2], ship.exit[3]},
+    color = "red",
+    size = 5
+})
+
+-- 生成具有任务的agv（取货）
+function generateagv()
+    -- 生成有箱子位置的列表
+    local availablepos = {}
+    for i = 1, cy.row do
+        for j = 1, cy.col do
+            for k = cy.level, 1, -1 do -- 只要最高层的箱子
+                local found = false -- 本次循环预定了最高层的箱子
+                -- 对应位置有集装箱且没有被预定
+                if cy.containers[i][j][k] ~= nil and cy.containers[i][j][k].reserved == nil then
+                    table.insert(availablepos, {i, j, k})
+                    found = true
+                end
+                if found then
+                    break
+                end
+            end
+        end
+    end
+
+    -- 判断堆场是否有箱子，如果没有则停止
+    if #availablepos == 0 and runcommand then
+        return
+    end
+
+    -- 随机抽取一个位置，生成agv
+    local pos = availablepos[math.random(#availablepos)]
+    cy.containers[pos[1]][pos[2]][pos[3]].reserved = true -- 标记为已经被预定
+    local agv = AGV()
+    agv:bindCrane(cy, pos)
+    print("[agv] summoned at: ", coroutine.qtime())
+
+    local tArriveSpan = math.random(agvSummonSpan) + 1 -- 平均到达间隔120s
+    print("[agv] next summon span: ", tArriveSpan, "s")
+    coroutine.queue(tArriveSpan, generateagv)
+end
+
+-- 判断所有任务是否执行完成
+function havetask()
+    for i = 1, #actionobj do
+        if #actionobj[i].tasksequence > 0 then
+            return true
+        end
+    end
+    return false
+end
+
+function recycle(obj)
+    if obj.type == "agv" then
+        if obj.container ~= nil then
+            obj.container:delete()
+        end
+        obj:delete()
+    end
+end
+
+-- 初始时间
+local t = os.clock()
+local dt = 0
+
+function update()
+    if runcommand then
+        coroutine.queue(dt, update)
+    end
+
+    -- 计算最大更新时间
+    local maxstep = math.huge
+    for i = 1, #actionobj do
+        if #actionobj[i].tasksequence > 0 then
+            maxstep = math.min(maxstep, actionobj[i]:maxstep())
+        end
+    end
+
+    -- 执行更新
+    for i = 1, #actionobj do
+        actionobj[i]:executeTask(dt)
+    end
+
+    -- 回收
+    for i = 1, #actionobj do
+        local obj = actionobj[i]
+
+        if obj.type == "agv" and #obj.tasksequence == 0 then
+            recycle(obj)
+            table.remove(actionobj, i)
+            break -- 假设每次同时只能到达一个，因此可以中止
+        end
+    end
+
+    -- 绘图
+    runcommand = scene.render()
+
+    -- 刷新时间间隔
+    dt = (os.clock() - t) * simv
+    dt = math.min(dt, maxstep)
+    t = os.clock()
+end
+
+update()
+
+-- 生成agv
+generateagv()
--- main.lua
+++ main.lua
@@ -1,145 +1,324 @@
+-- 控制器
 scene.setenv({
     grid = 'plane'
 })
 
--- 程序控制
-local runcommand = true
-
--- 参数设置
-local simv = 10 -- 仿真速度
-local actionobj = {} -- 动作队列声明
-local agvSummonSpan = 46 -- agv生成间隔
-
 -- 引用组件
+require('cy')
 require('rmg')
 require('agv')
-require('cy')
-require('rmgqc')
+require('node')
+require('road')
 require('ship')
+require('rmgqc')
 
--- 创建堆场
-local cy = CY({19.66 / 2, 51.49 / 2}, {-19.66 / 2, -51.49 / 2}, 3)
+-- 参数设置
+local simv = 4 -- 仿真速度
+local ActionObjs = {} -- 动作队列声明
 
--- 分配堆场给场桥
-local rmg = RMG(cy, actionobj)
-table.insert(actionobj, rmg)
+-- 仿真控制
+require('watchdog')
+local watchdog = WatchDog(simv, ActionObjs)
 
-local rmgqc = RMGQC() -- 获取岸桥
-rmg.nextstep = rmgqc -- 设置岸桥(仅在main中发现这样的作法)
-table.insert(actionobj, rmgqc)
+-- 创建道路系统
+local RoadList = {} -- 道路列表
+local NodeList = {} -- 节点列表
 
-local ship = SHIP(rmgqc)
-ship:initqueue()
+-- 创建节点
+local node1 = Node({0, 0, 10}, NodeList)
+local node2 = Node({0, 0, 50}, NodeList)
+local node3 = Node({20, 0, 50}, NodeList)
+local node4 = Node({20, 0, 160}, NodeList)
+local node5 = Node({0, 0, 160}, NodeList)
+local node6 = Node({-30, 0, 160}, NodeList)
+local node7 = Node({-30, 0, 280}, NodeList)
+local node8 = Node({0, 0, 280}, NodeList)
+local node9 = Node({0, 0, 300}, NodeList)
+-- 创建道路
+local rd1 = node1:createRoad(node2, RoadList)
+local rd2 = node2:createRoad(node3, RoadList)
+local rd3 = node3:createRoad(node4, RoadList)
+local rd4 = node4:createRoad(node5, RoadList)
+local rd5 = node2:createRoad(node5, RoadList)
+local rd6 = node5:createRoad(node6, RoadList)
+local rd7 = node6:createRoad(node7, RoadList)
+local rd8 = node7:createRoad(node8, RoadList)
+local rd9 = node5:createRoad(node8, RoadList)
+local rd10 = node8:createRoad(node9, RoadList)
 
-local line = scene.addobj("polyline", {
-    vertices = {cy.summon[1], cy.summon[2], cy.summon[3], ship.exit[1], ship.exit[2], ship.exit[3]},
-    color = "black"
-})
-local pt = scene.addobj("points", {
-    vertices = {cy.summon[1], cy.summon[2], cy.summon[3], ship.exit[1], ship.exit[2], ship.exit[3]},
-    color = "red",
-    size = 5
-})
+-- 创建堆场和rmg
+local cy = CY({45, 70}, {25, 140}, 3)
+cy:bindRoad(rd3)
+cy:showBindingPoint()
+cy:fillRandomContainerPositions(50, {'/res/ct/container_blue.glb'})
 
--- 生成具有任务的agv（取货）
+local rmg = RMG(cy, ActionObjs) -- 创建rmg时会自动添加到ActionObjs中
+
+-- 创建船和rmgqc
+local rmgqc = RMGQC({-30, 0, 210}, ActionObjs)
+local ship = Ship({8, 9, 2}, rmgqc.berthPosition)
+
+rmgqc:bindRoad(rd7) -- 绑定road
+rmgqc:bindShip(ship) -- 绑定Ship
+rmgqc:showBindingPoint()
+
+-- ship填充集装箱
+ship:fillRandomContainerPositions(30, {'/res/ct/container_blue.glb'})
+
+scene.render()
+
+local containerUrls = {'/res/ct/container.glb', '/res/ct/container_brown.glb', '/res/ct/container_blue.glb',
+                       '/res/ct/container_yellow.glb'} -- 集装箱模型路径列表（从其他文件中复制过来的）
+
+local generateConfig = {
+    ship = ship,
+    summonNum = 50,
+    averageSummonSpan = 15
+}
+
+function agv2rmgTask(agv, targetPos)
+    -- agv移动到目标位置(以后由controller调度)
+    agv:addtask({'moveon', {
+        road = rd1
+    }})
+    agv:addtask({'onnode', {node2, rd1, rd2}})
+    agv:addtask({'moveon', {
+        road = rd2
+    }})
+    agv:addtask({'onnode', {node3, rd2, rd3}})
+    agv:addtask({'moveon', {
+        road = rd3,
+        targetDistance = rmg.cy.parkingSpaces[targetPos[1]].relativeDist,
+        stay = true
+    }})
+    if agv.taskType == 'unload' then
+        agv:addtask({'detach', nil})
+        agv:addtask({'waitoperator', {agv.taskType}})
+    else
+        agv:addtask({'waitoperator', {agv.taskType}})
+        agv:addtask({'attach', nil})
+    end
+    agv:addtask({'moveon', {
+        road = rd3,
+        distance = rmg.cy.parkingSpaces[targetPos[1]].relativeDist,
+        stay = false
+    }})
+    agv:addtask({'onnode', {node4, rd3, rd4}})
+    agv:addtask({'moveon', {
+        road = rd4
+    }})
+    agv:addtask({'onnode', {node5, rd4, rd9}})
+    agv:addtask({'moveon', {
+        road = rd9
+    }})
+    agv:addtask({'onnode', {node8, rd9, rd10}})
+    agv:addtask({'moveon', {
+        road = rd10
+    }})
+    agv:addtask({'onnode', {node9, rd10, nil}})
+
+    rmg:registerAgv(agv)
+end
+
+function agv2rmgqcTask(agv, targetPos)
+    -- agv移动到目标位置(以后由controller调度)
+    agv:addtask({'moveon', {
+        road = rd1
+    }})
+    agv:addtask({'onnode', {node2, rd1, rd5}})
+    agv:addtask({'moveon', {
+        road = rd5
+    }})
+    agv:addtask({'onnode', {node5, rd5, rd6}})
+    agv:addtask({'moveon', {
+        road = rd6
+    }})
+    agv:addtask({'onnode', {node6, rd6, rd7}})
+    agv:addtask({'moveon', {
+        road = rd7,
+        targetDistance = rmgqc.parkingSpaces[targetPos[1]].relativeDist,
+        stay = true
+    }})
+    if agv.taskType == 'unload' then
+        agv:addtask({'detach', nil})
+        agv:addtask({'waitoperator', {agv.taskType}})
+    else
+        agv:addtask({'waitoperator', {agv.taskType}})
+        agv:addtask({'attach', nil})
+    end
+    agv:addtask({'moveon', {
+        road = rd7,
+        distance = rmgqc.parkingSpaces[targetPos[1]].relativeDist,
+        stay = false
+    }})
+    agv:addtask({'onnode', {node7, rd7, rd8}})
+    agv:addtask({'moveon', {
+        road = rd8
+    }})
+    agv:addtask({'onnode', {node8, rd8, rd10}})
+    agv:addtask({'moveon', {
+        road = rd10
+    }})
+    agv:addtask({'onnode', {node9, rd10, nil}})
+
+    rmgqc:registerAgv(agv)
+end
+
+function agv2rmg2rmgqcTask(agv, targetPos)
+    -- agv移动到目标位置(以后由controller调度)
+    agv:addtask({'moveon', {
+        road = rd1
+    }})
+    agv:addtask({'onnode', {node2, rd1, rd2}})
+    agv:addtask({'moveon', {
+        road = rd2
+    }})
+    agv:addtask({'onnode', {node3, rd2, rd3}})
+    agv:addtask({'moveon', {
+        road = rd3,
+        targetDistance = rmg.cy.parkingSpaces[targetPos[1]].relativeDist,
+        stay = true
+    }})
+    if agv.taskType == 'unload' then
+        agv:addtask({'detach', nil})
+        agv:addtask({'waitoperator', {agv.taskType}})
+    else
+        agv:addtask({'waitoperator', {agv.taskType}})
+        agv:addtask({'attach', nil})
+    end
+    agv:addtask({'moveon', {
+        road = rd3,
+        distance = rmg.cy.parkingSpaces[targetPos[1]].relativeDist,
+        stay = false
+    }})
+    agv:addtask({'onnode', {node4, rd3, rd4}})
+    agv:addtask({'moveon', {
+        road = rd4
+    }})
+    agv:addtask({'onnode', {node5, rd4, rd6}})
+    agv:addtask({'moveon', {
+        road = rd6
+    }})
+    agv:addtask({'onnode', {node6, rd6, rd7}})
+    agv:addtask({'moveon', {
+        road = rd7,
+        targetDistance = rmgqc.parkingSpaces[targetPos[1]].relativeDist,
+        stay = true
+    }})
+    if agv.taskType == 'unload' then
+        agv:addtask({'detach', nil})
+        agv:addtask({'waitoperator', {agv.taskType}})
+    else
+        agv:addtask({'waitoperator', {agv.taskType}})
+        agv:addtask({'attach', nil})
+    end
+    agv:addtask({'moveon', {
+        road = rd7,
+        distance = rmgqc.parkingSpaces[targetPos[1]].relativeDist,
+        stay = false
+    }})
+    agv:addtask({'onnode', {node7, rd7, rd8}})
+    agv:addtask({'moveon', {
+        road = rd8
+    }})
+    agv:addtask({'onnode', {node8, rd8, rd10}})
+    agv:addtask({'moveon', {
+        road = rd10
+    }})
+    agv:addtask({'onnode', {node9, rd10, nil}})
+    rmg:registerAgv(agv)
+end
+
+-- 生成具有任务的agv(ship)
 function generateagv()
-    -- 生成有箱子位置的列表
-    local availablepos = {}
-    for i = 1, cy.row do
-        for j = 1, cy.col do
-            for k = cy.level, 1, -1 do -- 只要最高层的箱子
-                local found = false -- 本次循环预定了最高层的箱子
-                -- 对应位置有集装箱且没有被预定
-                if cy.containers[i][j][k] ~= nil and cy.containers[i][j][k].reserved == nil then
-                    table.insert(availablepos, {i, j, k})
-                    found = true
+
+    local dataModel = math.random() > 0.5 and cy or ship
+    -- 获取位置可用箱数信息，如果没有则注入
+    -- 只有generateagv函数中能存取集装箱，因此只有此处设置positionLevels
+    if dataModel.positionLevels == nil then
+        dataModel.positionLevels = {} -- 初始化集装箱可用位置列表
+
+        for i = 1, dataModel.bay do
+            dataModel.positionLevels[i] = {}
+            for j = 1, dataModel.row do
+                -- 计算此位置的堆叠层数
+                local levelCount = 0
+                for k = 1, dataModel.level do
+                    if dataModel.containers[i][j][k] ~= nil then
+                        levelCount = levelCount + 1
+                    end
                 end
-                if found then
-                    break
+                dataModel.positionLevels[i][j] = levelCount
+            end
+        end
+    end
+
+    -- 生成agv任务类型
+    local agvTaskType = math.random(2) == 1 and 'unload' or 'load' -- 1:agv卸货，2:agv装货
+    print('生成AGV，taskType=', agvTaskType)
+
+    -- 识别任务类型并生成可用位置列表
+    local availablePos = {} -- 可用位置
+    for i = 1, dataModel.bay do
+        for j = 1, dataModel.row do
+            local containerLevel = dataModel.positionLevels[i][j] -- 获取堆叠层数
+            if agvTaskType == 'unload' then
+                -- agv卸货，找到所有可用的存货位置(availableNum < dataModel.level)
+                if containerLevel < dataModel.level then
+                    table.insert(availablePos, {i, j, containerLevel + 1}) -- 记录可用位置{bay, row}
+                end
+            else
+                -- agv装货，找到所有可用的取货位置(availableNum > 0)
+                if containerLevel > 0 then
+                    table.insert(availablePos, {i, j, containerLevel}) -- 记录可用位置{bay, row}
                 end
             end
         end
     end
 
-    -- 判断堆场是否有箱子，如果没有则停止
-    if #availablepos == 0 and runcommand then
+    local targetPos = availablePos[math.random(#availablePos)] -- 抽取可用位置
+    -- 记录抽取位置的影响
+    local trow, tcol = targetPos[1], targetPos[2]
+    dataModel.positionLevels[trow][tcol] = dataModel.positionLevels[trow][tcol] + (agvTaskType == 'unload' and 1 or -1)
+
+    -- 生成agv
+    local agv = AGV()
+    agv.taskType = agvTaskType -- 设置agv任务类型(unload/load)
+    if agv.taskType == 'unload' then -- agv卸货，生成集装箱
+        agv.container = scene.addobj(containerUrls[1]) -- 生成agv携带的集装箱
+    end
+    agv:move2(10, 0, -10)
+    agv.targetContainerPos = targetPos -- 设置agv目标位置{bay,row,col}
+    agv:bindCrane(dataModel, targetPos) -- 绑定agv和船
+
+    -- agv添加任务
+    if dataModel.type == 'cy' then
+        -- 直接到rmg
+        agv2rmgTask(agv, targetPos)
+        print('[main] agv', agv.id, '2rmg')
+    elseif dataModel.type == 'ship' then
+        agv2rmgqcTask(agv, targetPos)
+        print('[main] agv', agv.id, '2rmgqc')
+    else
+        print('[main] error: unknown dataModel type', dataModel.type, '. stopped.')
+        os.exit()
+    end
+
+    print('[main] agv target=', agv.targetContainerPos[1], agv.targetContainerPos[2], agv.targetContainerPos[3],
+        ', agv taskType=', agv.taskType)
+
+    -- 程序控制
+    if not watchdog.runcommand or generateConfig.summonNum == 0 then
         return
     end
+    generateConfig.summonNum = generateConfig.summonNum - 1 -- agv剩余生成次数减1
 
-    -- 随机抽取一个位置，生成agv
-    local pos = availablepos[math.random(#availablepos)]
-    cy.containers[pos[1]][pos[2]][pos[3]].reserved = true -- 标记为已经被预定
-    local agv = AGV()
-    agv:bindCrane(cy, pos)
-    print("[agv] summoned at: ", coroutine.qtime())
-
-    local tArriveSpan = math.random(agvSummonSpan) + 1 -- 平均到达间隔120s
-    print("[agv] next summon span: ", tArriveSpan, "s")
+    -- 添加事件
+    print("[agv", agv.roadAgvId or agv.id, "] summon at: ", coroutine.qtime())
+    local tArriveSpan = math.random(generateConfig.averageSummonSpan)
     coroutine.queue(tArriveSpan, generateagv)
 end
-
--- 判断所有任务是否执行完成
-function havetask()
-    for i = 1, #actionobj do
-        if #actionobj[i].tasksequence > 0 then
-            return true
-        end
-    end
-    return false
-end
-
-function recycle(obj)
-    if obj.type == "agv" then
-        if obj.container ~= nil then
-            obj.container:delete()
-        end
-        obj:delete()
-    end
-end
-
--- 初始时间
-local t = os.clock()
-local dt = 0
-
-function update()
-    if runcommand then
-        coroutine.queue(dt, update)
-    end
-
-    -- 计算最大更新时间
-    local maxstep = math.huge
-    for i = 1, #actionobj do
-        if #actionobj[i].tasksequence > 0 then
-            maxstep = math.min(maxstep, actionobj[i]:maxstep())
-        end
-    end
-
-    -- 执行更新
-    for i = 1, #actionobj do
-        actionobj[i]:executeTask(dt)
-    end
-
-    -- 回收
-    for i = 1, #actionobj do
-        local obj = actionobj[i]
-
-        if obj.type == "agv" and #obj.tasksequence == 0 then
-            recycle(obj)
-            table.remove(actionobj, i)
-            break -- 假设每次同时只能到达一个，因此可以中止
-        end
-    end
-
-    -- 绘图
-    runcommand = scene.render()
-
-    -- 刷新时间间隔
-    dt = (os.clock() - t) * simv
-    dt = math.min(dt, maxstep)
-    t = os.clock()
-end
-
-update()
-
--- 生成agv
 generateagv()
+
+-- 仿真任务
+watchdog:update()
