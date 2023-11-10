3acdb38b1a8ea87d074fb031f483b13122a5d20a
huuhghhgyg <huuhghhgyg@outlook.com>
2023-11-10 17:31:34 +0800

    [feat] 添加道路规划

--- controller.lua
+++ controller.lua
@@ -32,16 +32,66 @@ function Controller()
         if node2.fromNodesId == nil or node2.toNodesId == nil then
             print('[controller] 警告：node', node2.id, '不是从Controller中创建的，没有fromNodesId或toNodesId属性')
         end
-        
+
         -- 添加OD信息
-        table.insert(node1.toNodesId, node2.id) -- node1中的去向节点ID列表中添加node2的id
+        table.insert(node1.toNodesId, node2.id)   -- node1中的去向节点ID列表中添加node2的id
         table.insert(node2.fromNodesId, node1.id) -- node2中的来源节点ID列表中添加node1的id
 
         return node1:createRoad(node2, controller.Roads)
     end
 
-    function controller:createNetwork()
+    function controller:shortestPath(fromNodeId, toNodeId)
+        local stp = math.newmip() -- 创建线性规划问题
 
+        -- 设置目标函数
+        local functionCoeffs = {} -- 目标函数系数
+        for roadId, road in ipairs(controller.Roads) do
+            functionCoeffs[roadId] = road.length
+        end
+        stp:addrow(functionCoeffs, 'min') -- 添加目标函数行
+        print('targetfunc:\n', table.unpack(functionCoeffs))
+
+        -- 对每个点添加约束条件
+        for nodeId, node in ipairs(controller.Nodes) do
+            -- print('coeff of NodeId' .. nodeId)
+            local constraintCoeffs = {} -- 约束条件系数
+            for roadId, road in ipairs(controller.Roads) do
+                if road.fromNode == node then
+                    -- print('road', roadId, 'is fromNode', node.id)
+                    constraintCoeffs[roadId] = 1 -- 出路径
+                elseif road.toNode == node then
+                    -- print('road', roadId, 'is toNode', node.id)
+                    constraintCoeffs[roadId] = -1 -- 入路径
+                else
+                    constraintCoeffs[roadId] = 0  -- 不经过
+                end
+            end
+
+            local b = 0 -- 初始化约束右端项
+            -- 起始点判断
+            if node.id == fromNodeId then
+                b = 1  -- 始点净流量为1
+            elseif node.id == toNodeId then
+                b = -1 -- 终点净流量为-1
+            end
+
+            stp:addrow(constraintCoeffs, '==', b)
+            -- print(table.unpack(constraintCoeffs), '==', b)
+            local str = ''
+            for k, v in ipairs(constraintCoeffs) do
+                str = str .. v .. ' '
+            end
+            print(str, '==', b)
+        end
+        
+        -- 添加01变量
+        for roadId = 1, #controller.Roads do
+            stp:addrow('c' .. roadId, 'bin')
+            -- print('r' .. roadId, 'bin')
+        end
+        stp:solve()
+
+        return stp
     end
 
     return controller
