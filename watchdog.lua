0affe6289c02896f31c9eb134d123bcf70779535
huuhghhgyg <huuhghhgyg@outlook.com>
2023-11-21 11:55:53 +0800

    [fix] 修复maxstep没有应用

--- watchdog.lua
+++ watchdog.lua
@@ -7,23 +7,19 @@ function WatchDog(simv, ActionObjs)
         runcommand = true
     }
 
-    -- todo: 此处的控制程序修改过，可能需要更新到正式使用的文件中
     function watchdog:update()
-        -- 刷新时间间隔
+        -- 刷新运行时间间隔
         watchdog.dt = (os.clock() - watchdog.t) * simv
-        watchdog.t = os.clock()
+        watchdog.t = os.clock() -- 刷新update时间
 
-        -- 计算最大更新时间
-        local maxstep = watchdog.dt
-        for i = 1, #ActionObjs do
-            if #ActionObjs[i].tasksequence > 0 then
-                maxstep = math.min(maxstep, ActionObjs[i]:maxstep())
-            end
-        end
+        -- 绘图
+        watchdog.runcommand = scene.render()
 
-        -- 执行更新
-        for i = 1, #ActionObjs do
-            ActionObjs[i]:executeTask(watchdog.dt)
+        -- 检测暂停指令
+        if not watchdog.runcommand then
+            scene.render() -- 最后一次绘图
+            print('仿真推进停止')
+            return
         end
 
         -- 回收
@@ -37,13 +33,43 @@ function WatchDog(simv, ActionObjs)
             end
         end
 
-        -- 绘图
-        watchdog.runcommand = scene.render()
+        -- 计算最大更新时间
+        local maxstep = watchdog.dt
+        local laststep = watchdog.dt -- debug 严格模式
+        for i = 1, #ActionObjs do
+            if #ActionObjs[i].tasksequence > 0 then
+                maxstep = math.min(maxstep, ActionObjs[i]:maxstep())
+                -- 严格模式debug
+                if maxstep ~= laststep and maxstep < 0.000001 then
+                    print(ActionObjs[i].type, ActionObjs[i].id, ActionObjs[i].tasksequence[1][1], 'set maxstep=',
+                        maxstep, ' ----------------------------------------------------------')
+                    if ActionObjs[i].type == 'agv' and ActionObjs[i].tasksequence[1][1] == 'moveon' then
+                        print('agv', ActionObjs[i].id, 'moveon', ActionObjs[i].road.id)
+                    end
+                end
+                laststep = maxstep
+            end
+        end
+
+        watchdog.dt = maxstep -- 修正dt(严格模式)
+        -- watchdog.dt = maxstep > 0 and maxstep or watchdog.dt -- 修正dt
+
+        -- 执行更新
+        for i = 1, #ActionObjs do
+            ActionObjs[i]:executeTask(watchdog.dt)
+        end
+
+        if watchdog.dt < 0.0001 then
+            print('dt < 0.0001, dt=', watchdog.dt,
+                ' =================================================================')
+        end
 
         -- 下一次更新
-        if watchdog.runcommand then
-            coroutine.queue(watchdog.dt, watchdog.update)
-        end
+        coroutine.queue(watchdog.dt, watchdog.update)
+    end
+
+    function watchdog:queueAt(dt)
+        coroutine.queue(dt, watchdog.update)
     end
 
     function watchdog:recycle(obj)
@@ -51,6 +77,7 @@ function WatchDog(simv, ActionObjs)
             if obj.container ~= nil then
                 obj.container:delete()
             end
+            print('agv', obj.id, 'leave at ', coroutine.qtime())
             obj:delete()
         end
     end
