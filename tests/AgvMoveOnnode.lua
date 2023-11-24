e1d94f7bc4d218fc990b22bd8bfe83e3a50def7e
huuhghhgyg <huuhghhgyg@outlook.com>
2023-11-24 11:08:43 +0800

    [test] 新增Agv测试脚本

--- tests/AgvMoveOnnode.lua
+++ tests/AgvMoveOnnode.lua
@@ -20,16 +20,24 @@ local NodeList = {}
 
 local NodeDefaultRadius = 5
 local n1 = Node({0,0,0-NodeDefaultRadius}, NodeList)
-local n2 = Node({0,0,150+NodeDefaultRadius}, NodeList)
-local rd = n1:createRoad(n2, RoadList)
+local n2 = Node({0,0,50}, NodeList)
+local n3 = Node({0,0,100}, NodeList)
+local n4 = Node({50+NodeDefaultRadius,0,100}, NodeList)
+local rd1 = n1:createRoad(n2, RoadList)
+local rd2 = n2:createRoad(n3, RoadList)
+local rd3 = n3:createRoad(n4, RoadList)
 
 local agv = AGV()
 -- move2 测试
 -- agv:addtask('move2',{0,150})
 
 -- onnode 测试
-agv:addtask('moveon',{road=rd})
-agv:addtask('onnode',{n2,rd})
+agv:addtask('moveon',{road=rd1})
+agv:addtask('onnode',{n2,rd1,rd2})
+agv:addtask('moveon',{road=rd2})
+agv:addtask('onnode',{n2,rd2,rd3})
+agv:addtask('moveon',{road=rd3})
+agv:addtask('onnode',{n3,rd3})
 
 table.insert(ActionObjs, agv)
 
