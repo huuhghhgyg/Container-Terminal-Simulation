63674396fd57b0a9182818d8e7f979a7f0868ad9
huuhghhgyg <huuhghhgyg@outlook.com>
2024-01-17 00:03:03 +0800

    [test] 添加测试

---
+++ tests/RMGMove.lua
@@ -0,0 +1,21 @@
+-- 仿真流程部分
+scene.setenv({
+    grid = 'plane'
+})
+
+local actionObjs = {}
+local simv = 1
+
+require('rmg2')
+
+local rmg = RMG()
+rmg:addtask('move2', {0, 0, 20})
+rmg:addtask('move2', {0, 10, 20})
+rmg:addtask('move2', {-10, 10, 20})
+rmg:addtask('move2', {0, 0, 0})
+
+table.insert(actionObjs, rmg)
+
+require('watchdog')
+local watchdog = WatchDog(simv, actionObjs)
+watchdog.update()
